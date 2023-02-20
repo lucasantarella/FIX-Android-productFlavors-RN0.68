4438e6d2221f1576688ebf095d48cdab4f84a3bc
Luca Santarella <luca.santarella@gmail.com>
2023-02-20 17:47:11 -0500

    More linting cleanup

--- FIX-RN-productFlavors.js
+++ FIX-RN-productFlavors.js
@@ -2,9 +2,9 @@
  * @author 9christian9
  * @description This script file must be in the react native root project
  * <br> This script work only with RN 0.67.x and Android Gradle Plugin 7.2 - 7.3
-*/
+ */
 
-const {readFile, writeFile} = require('fs');
+const { readFile, writeFile } = require("fs");
 const reactNative = "./node_modules/react-native/react.gradle";
 
 const workaround = `
@@ -43,28 +43,28 @@ def currentAssetsCopyTask = tasks.create(
 
 `;
 
-readFile(reactNative, 'utf-8', function (err, contents) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    if(!contents.includes(workaround)){
-        contents = contents.split("\n");
-        contents.splice(288, 38);
-        contents[288] = [workaround];
-        contents = contents.join("\n");
+readFile(reactNative, "utf-8", function (err, contents) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  if (!contents.includes(workaround)) {
+    contents = contents.split("\n");
+    contents.splice(288, 38);
+    contents[288] = [workaround];
+    contents = contents.join("\n");
 
-        writeFile(reactNative, contents, 'utf-8', function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }else{
-                console.log('File react.gradle changed correctly');
-                return;
-            }
-        });
-    }else{
-        console.log('Fix already present');
-        return;
-    }
+    writeFile(reactNative, contents, "utf-8", function (err) {
+      if (err) {
+        console.log(err);
+
+      } else {
+        console.log("File react.gradle changed correctly");
+
+      }
+    });
+  } else {
+    console.log("Fix already present");
+
+  }
 });
